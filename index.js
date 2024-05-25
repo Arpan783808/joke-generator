1eb5e66ec5c26fd177bb09c82d55a83f74cb6593
Arpan Tomar <arpantomar2018@gmail.com>
2024-05-25 15:51:24 +0530

    joke gen

---
+++ index.js
@@ -0,0 +1,76 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+const app=express();
+
+app.use(express.json());
+
+app.set("view engine", "ejs");
+const API_URL = "https://v2.jokeapi.dev/joke";
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/getjoke",async (req,res)=>{
+    try{
+        const result=await axios.get(API_URL+"/Any");
+        
+        const joke=result.data;
+        console.log(joke);
+        // console.log(joke1);
+        res.render("index.ejs",{joke});
+    }
+    catch(error){
+        res.render("index.ejs",{error});
+    }
+})
+app.get("/getdark", async (req, res) => {
+  try {
+    const result = await axios.get(API_URL + "/Dark");
+
+    const joke = result.data;
+    console.log(joke);
+    // console.log(joke1);
+    res.render("index.ejs", { joke });
+  } catch (error) {
+    res.render("index.ejs", { error });
+  }
+});
+app.get("/getprog", async (req, res) => {
+  try {
+    const result = await axios.get(API_URL + "/Programming");
+
+    const joke = result.data;
+    console.log(joke);
+    // console.log(joke1);
+    res.render("index.ejs", { joke });
+  } catch (error) {
+    res.render("index.ejs", { error });
+  }
+});
+app.get("/getpun", async (req, res) => {
+  try {
+    const result = await axios.get(API_URL + "/Pun");
+
+    const joke = result.data;
+    console.log(joke);
+    // console.log(joke1);
+    res.render("index.ejs", { joke });
+  } catch (error) {
+    res.render("index.ejs", { error });
+  }
+});
+app.get("/getspooky", async (req, res) => {
+  try {
+    const result = await axios.get(API_URL + "/Spooky");
+
+    const joke = result.data;
+    console.log(joke);
+    // console.log(joke1);
+    res.render("index.ejs", { joke });
+  } catch (error) {
+    res.render("index.ejs", { error });
+  }
+});
+
+app.listen(5000,()=>{
+    console.log("app listening at port 5000");
+})
